61436cca88a1e8a9cdbb478eb1fc744bd28fcfa9
IETatGIKI <ietgiki2024@gmail.com>
2024-10-01 18:19:34 +0500

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -72,27 +72,27 @@ const Main = () => {
                             <h5 className='h5-one'>The institute of engineering and technology</h5><br /><br /><br />
                             <h3 className=''>" INTRODUCTION OF OUR EXECUTIVE COUNCIL"</h3>
                         </div>
-                        <div className={`${click === 0 ? 'col-md-8' : 'd-none'} one p-0`}>
+                        <div className={`${click === 10 ? 'col-md-8' : 'd-none'} one p-0`}>
                             <h1 className='h1-one'>PRESIDENT:</h1>
                             <h5 className='h5-one'>MOHAMMAD HARIS</h5><br /><br /><br />
                             <h3 className='p-one'>" Baqi jesy apki marzi "</h3>
                         </div>
-                        <div className={`${click === 1 ? 'col-md-8' : 'd-none'} two`}>
+                        <div className={`${click === 9 ? 'col-md-8' : 'd-none'} two`}>
                             <h1 className='h1-one'>EVENT COORDINATOR:</h1>
                             <h5 className='h5-one'>HUSSAIN AHMED VIRK:</h5><br />
                             <h3 className='p-one'>" CHEEN TAPAK DUM DUM "</h3>
                         </div>
-                        <div className={`${click === 2 ? 'col-md-8' : 'd-none'} three`}>
+                        <div className={`${click === 8 ? 'col-md-8' : 'd-none'} three`}>
                             <h1 className='h1-one'>GENERAL SECRETARY:</h1>
                             <h5 className='h5-one'>AMMAD SHAHID:</h5><br />
                             <h3 className='p-one'>" MEETING MEIN NA AY TO DIRECT TERMINATION "</h3>
                         </div>
-                        <div className={`${click === 3 ? 'col-md-8' : 'd-none'} four`}>
+                        <div className={`${click === 7 ? 'col-md-8' : 'd-none'} four`}>
                             <h1 className='h1-one'>VICE PRESIDENT:</h1>
                             <h5 className='h5-one'>MURTAZA SAEED:</h5><br />
                             <h3 className='p-one'>"YOU SHOULD KNOW BETTER"</h3>
                         </div>
-                        <div className={`${click === 4 ? 'col-md-8' : 'd-none'} five`}>
+                        <div className={`${click === 6 ? 'col-md-8' : 'd-none'} five`}>
                             <h1 className='h1-one'>COORDINATOR INTRA:</h1>
                             <h5 className='h5-one'>ARBAB ASLAM:</h5><br />
                             <h3 className='p-one'>" GIKI OLX JOIN KARLENA " </h3>
@@ -102,27 +102,27 @@ const Main = () => {
                             <h5 className='h5-one'>M.UMER FAROOQ:</h5><br />
                             <h3 className='p-one'>"Dream big, act small, and stay humble."</h3>
                         </div>
-                        <div className={`${click === 6 ? 'col-md-8' : 'd-none'} seven`}>
+                        <div className={`${click === 4 ? 'col-md-8' : 'd-none'} seven`}>
                             <h1 className='h1-one'>TRESURER:</h1>
                             <h5 className='h5-one'>ZAFIR SALAHUDDIN:</h5><br />
                             <h3 className='p-one'>"Your only limit is you."</h3>
                         </div>
-                        <div className={`${click === 7 ? 'col-md-8' : 'd-none'} eight`}>
+                        <div className={`${click === 3 ? 'col-md-8' : 'd-none'} eight`}>
                             <h1 className='h1-one'>DIRECTOR LIASON :</h1>
                             <h5 className='h5-one'>KOMAL JAVAID:</h5><br />
                             <h3 className='p-one'>"Shine your light, even on the darkest days."</h3>
                         </div>
-                        <div className={`${click === 8 ? 'col-md-8' : 'd-none'} nine`}>
+                        <div className={`${click === 2 ? 'col-md-8' : 'd-none'} nine`}>
                             <h1 className='h1-one'>DIRECTOR GIS:</h1>
                             <h5 className='h5-one'>M.KAMIL AYUB:</h5><br />
                             <h3 className='p-one'>" YOU ONLY LOOSE WHEN YOU GIVE UP "</h3>
                         </div>
-                        <div className={`${click === 9 ? 'col-md-8' : 'd-none'} ten`}>
+                        <div className={`${click === 1 ? 'col-md-8' : 'd-none'} ten`}>
                             <h1 className='h1-one m-0 mb-1'>DIRECTOR GENERAL:</h1>
                             <h5 className='h5-one m-0'>MOHAMMAD TALHA:</h5><br />
                             <h3 className='p-one p-0 m-0'>"SCENE BAS 2 HE HAIN : <br /> (Good scene aur bad scene) "</h3>
                         </div>
-                        <div className={`${click === 10 ? 'col-md-8' : 'd-none'} ten`}>
+                        <div className={`${click === 0 ? 'col-md-8' : 'd-none'} ten`}>
                             <h1 className='h1-one m-0 mb-1'>EXECUTIVE MEMBER:</h1>
                             <h5 className='h5-one m-0'>EMAN ANJUM:</h5><br />
                             <h3 className='p-one p-0 m-0 w-75'>"KEEP IT REAL KEEP IT KIND"</h3>
